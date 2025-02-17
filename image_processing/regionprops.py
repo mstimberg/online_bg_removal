7f54fb269d94f7c1580dc6321c7314f62907c47d
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2025-02-17 18:02:18 +0100

    Initial commit

---
+++ image_processing/regionprops.py
@@ -0,0 +1,254 @@
+"""
+Segmentation and region properties of images.
+
+Uses cupy and cucim for processing on the GPU
+
+Frames are stacked horizontally on the GPU, to use the GPU memory efficiently.
+"""
+from skimage.measure import _moments
+import numpy as np
+
+try:
+    import cupy as cp
+    cp.cuda.runtime.getDevice()
+except Exception:
+    print("Regionprops: Cupy/GPU not available, falling back to CPU")
+    cp = None
+
+if cp:
+    xp = cp
+    from cucim.skimage.measure._label import (
+        _get_structure, _label
+    )
+
+    STRUCTURE = _get_structure(2, 1)
+
+    # Bincount kernel that ignores the background label
+    _no_zero_bincount_kernel = cp.ElementwiseKernel(
+        'S x', 'raw U bin',
+        '''
+        if (x > 0)
+            atomicAdd(&bin[x], U(1));
+        ''',
+        'no_zero_bincount_kernel')
+else:
+    xp = np
+    from skimage.measure._label import label
+
+
+
+def find_objects(labels):
+    """
+    Find objects in an image based on labeled regions.
+
+    Parameters
+    ----------
+    labels: ndarray
+        Array containing labeled regions.
+
+    Returns
+    -------
+    slices: list
+        List of slices representing the bounding boxes of each object.
+    unique_labels: ndarray
+        Array containing the unique labels of the objects.
+    """
+    non_zero_indices = xp.nonzero(labels)
+    values = labels[non_zero_indices]
+    unique_labels = xp.unique(values)
+
+    # Try keeping everything on the GPU as long as possible
+    label_matches = values[None, :] == unique_labels[:, None]
+    # FIXME: this weird type cast is needed to work around a cupy compilation bug on Windows
+    if cp:
+        p = cp.hstack([0, cp.cumsum(cp.sum(label_matches.astype(cp.float32), axis=1).astype(cp.int32))])
+    else:
+        p = np.hstack([0, np.cumsum(np.sum(label_matches, axis=1))])
+    r = xp.broadcast_to(non_zero_indices[0], label_matches.shape)[label_matches]
+    c = xp.broadcast_to(non_zero_indices[1], label_matches.shape)[label_matches]
+
+    if cp:
+        objects = p[1:-1].get()
+    else:
+        objects = p[1:-1]
+    r_split = xp.split(r, objects)
+    c_split = xp.split(c, objects)
+    if r_split[0].size == 0:
+        return []
+    slices = [
+        (
+            slice(r_split_element.min().item(), r_split_element.max().item() + 1),
+            slice(c_split_element.min().item(), c_split_element.max().item() + 1),
+        )
+        for r_split_element, c_split_element in zip(r_split, c_split)
+    ]
+    return slices
+
+def determine_labels(buffer, min_area_pixels, max_area_pixels):
+    """
+    Determine labels for connected components in a binary image based on their area.
+
+    Parameters
+    ----------
+    buffer : cp.ndarray or np.ndarray
+        The input binary image.
+    min_area_pixels : int
+        The minimum area (in pixels) for a connected component to be considered valid.
+    max_area_pixels : int
+        The maximum area (in pixels) for a connected component to be considered valid.
+    """
+    # binarize the image
+    binary_image = (buffer.max() - buffer) > 0
+    # label the connected components    
+
+    if cp:
+        labels = cp.empty_like(binary_image, dtype=cp.int32)
+        _label(binary_image, STRUCTURE, labels, greyscale_mode=False) 
+    else:
+        labels = label(binary_image, connectivity=1)
+
+    # filter out connected components based on their area    
+    if cp:
+        component_sizes = xp.zeros(int(labels.max()) + 1, dtype=xp.intp)
+        _no_zero_bincount_kernel(labels.ravel(), component_sizes)
+    else:
+        component_sizes = np.bincount(labels.ravel(), minlength=int(labels.max()) + 1)
+    wrong_size = (component_sizes < min_area_pixels) | (component_sizes > max_area_pixels)
+    wrong_size_mask = wrong_size[labels]
+    labels[wrong_size_mask] = 0
+    return labels
+
+def determine_images(labels):
+    objects = find_objects(labels)
+    if cp:
+        images = [(labels[sl] != 0).get() for sl in objects]
+    else:
+        images = [(labels[sl] != 0) for sl in objects]
+    indices_all = [np.nonzero(image) for image in images]
+    return images, objects, indices_all
+
+def determine_properties(labels, start_frame_idx, regionprops=("bbox", "ellipse")):
+    """
+    Determine region properties of connected components in a labeled image.
+
+    Parameters
+    ----------
+    labels : cp.ndarray or np.ndarray
+        The labeled image.
+    start_frame_idx : int
+        The index of the first frame in the image sequence.
+    frame_width : int
+        The width of a frame in pixels.
+    regionprops : tuple
+        A tuple containing the region properties to calculate. Supported values are 'bbox', 'ellipse', and 'orientation'.
+        The centroid is always calculated.
+    
+    Returns
+    -------
+    properties : dict
+        A dictionary containing the requested region properties. Centroid and frame index are always included.
+        The keys follow the scikit-image convention for multi-dimensional properties, e.g. the centroid is stored
+        as 'centroid-0' and 'centroid-1'.
+    """
+    images, objects, indices_all = determine_images(labels)
+
+    properties = extract_properties(start_frame_idx, images, objects, indices_all, regionprops)
+
+    return properties
+
+def extract_properties(start_frame_idx, images, objects, indices_all, regionprops=("bbox", "ellipse", "orientation")):
+    if "orientation" in regionprops and "ellipse" not in regionprops:
+        raise ValueError("The 'orientation' property can only be calculated if 'ellipse' is requested.")
+    if not len(indices_all):
+        results = {
+            "frame": np.array([], dtype=int),
+            "centroid-0": np.array([], dtype=float),
+            "centroid-1": np.array([], dtype=float),
+        }
+        if "bbox" in regionprops:
+            results["bbox-0"] = np.array([], dtype=int)
+            results["bbox-1"] = np.array([], dtype=int)
+            results["bbox-2"] = np.array([], dtype=int)
+            results["bbox-3"] = np.array([], dtype=int)
+        if "ellipse" in regionprops:
+            if 'orientation' in regionprops:
+                results["orientation"] = np.array([], dtype=float)
+            results["minor_axis_length"] = np.array([], dtype=float)
+            results["major_axis_length"] = np.array([], dtype=float)
+        return results
+    
+
+    if "ellipse" not in regionprops:  # For ellipses, we already create the moments
+        centroid = [
+            np.vstack(
+                [
+                    indices[0] + sl[0].start,
+                    indices[1] + sl[1].start,
+                ]
+            ).mean(axis=1)
+            for indices, sl in zip(indices_all, objects)
+        ]
+
+    if "bbox" in regionprops:
+        bboxes = [np.array([sl[0].start, sl[1].start, sl[0].stop, sl[1].stop], dtype=np.int32) for sl in objects]
+
+    if "ellipse" in regionprops:
+        M_all = [
+            _moments.moments(image, 3) for image in images
+        ]
+        
+        centroid_local = [M[tuple(np.eye(2, dtype=int))] / M[(0,) * 2] for M in M_all]
+        centroid = [np.array([sl[0].start, sl[1].start]) + c for sl, c in zip(objects, centroid_local)]
+
+        mu_all = [
+            _moments.moments_central(
+                image,
+                c,
+                order=2,
+            )
+            for image, c in zip(images, centroid_local)
+        ]
+
+        inertia_tensor_all = [
+            _moments.inertia_tensor(image, mu)
+            for image, mu in zip(images, mu_all)
+        ]
+        inertia_tensor_eigvals_all = [
+            _moments.inertia_tensor_eigvals(image, T=inertia_tensor)
+            for image, inertia_tensor in zip(images, inertia_tensor_all)
+        ]
+
+        inertia_tensor_eigvals_all = np.stack(inertia_tensor_eigvals_all)
+        major_axis_length = 4 * np.sqrt(inertia_tensor_eigvals_all[:, 0])
+        minor_axis_length = 4 * np.sqrt(inertia_tensor_eigvals_all[:, -1])
+        
+        if 'orientation' in regionprops:
+            inertia_tensor_all = np.stack(inertia_tensor_all).reshape(-1, 4)
+            a, b, b, c = inertia_tensor_all.T
+            orientation = np.where(a - c == 0, np.where(b < 0, -np.pi/4, np.pi/4), np.arctan2(2 * b, a - c) / 2)
+    
+    properties = {}
+
+    # Centroid and frame information is always stored
+    centroid = np.stack(centroid)
+    properties["frame"] = np.array([start_frame_idx for _ in objects])
+    properties["centroid-0"] = centroid[:, 0]
+    properties["centroid-1"] = centroid[:, 1]
+
+    # Bounding boxes
+    if "bbox" in regionprops:
+        bboxes = np.stack(bboxes)
+        # Does it make sense to store bounding boxes in µm instead of pixels ?
+        # Here this is mostly for visualization with napari
+        properties["bbox-0"] = bboxes[:, 0]
+        properties["bbox-1"] = bboxes[:, 1]
+        properties["bbox-2"] = bboxes[:, 2]
+        properties["bbox-3"] = bboxes[:, 3]
+
+    # Ellipse information (orientation and two axes)
+    if "ellipse" in regionprops:
+        properties["minor_axis_length"] = minor_axis_length
+        properties["major_axis_length"] = major_axis_length
+        if 'orientation' in regionprops:
+            properties["orientation"] = orientation
+    return properties
---
+++ image_processing/tracker.py
@@ -0,0 +1,300 @@
+import collections
+from datetime import datetime
+import time
+import glob
+import gzip
+import logging
+from logging.handlers import RotatingFileHandler
+from multiprocessing import Process
+from concurrent.futures import ThreadPoolExecutor
+import concurrent
+import os
+import shutil
+
+import numpy as np
+try:
+    import cupy as cp
+    cp.cuda.runtime.getDevice()
+except Exception:
+    print("Tracker: Cupy/GPU not available, falling back to CPU")
+    cp = None
+
+import pandas as pd
+import imageio.v3 as imageio
+
+from image_processing.regionprops import determine_labels, determine_images, extract_properties
+
+TRACK_IN_PARALLEL = 8
+TRIES = 10  # Number of times to try tracking a frame before giving up
+
+CONSOLE_LOG_LEVEL = logging.WARNING
+FILE_LOG_LEVEL = logging.INFO
+
+# Make sure that log messages always have an index
+def default_index_filter(record: logging.LogRecord):
+    record.index = record.index if hasattr(record, "index") else -1
+    return record
+
+def log_namer(name):
+    return name + ".gz"
+
+def log_rotator(source, dest):
+    with open(source, 'rb') as f_in:
+        with gzip.open(dest, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove(source)
+
+
+class Tracker(Process):
+    def __init__(self, pixel_size, min_area, max_area, target_folder, roi_size, ellipse, orientation, track_tasks, link, zip_tracking_file, track_settings):
+        super().__init__()
+        self.pixel_size = pixel_size
+        self.frame_rate = track_settings["frame_rate"]
+        del track_settings["frame_rate"]
+        self.min_area_pixels = min_area/pixel_size**2
+        self.max_area_pixels = max_area/pixel_size**2
+        self.target_folder = target_folder
+        self.roi_size = roi_size
+        self.link = link
+        self.zip_tracking_file = zip_tracking_file
+        self.track_settings = track_settings
+        props = ["bbox"]
+        if ellipse:
+            props.append("ellipse")
+        if orientation:
+            props.append("orientation")
+        self.regionprops = tuple(props)
+        self.track_tasks = track_tasks
+        self.columns = None
+        self.cuda_stream = None
+        self.start_time = None
+        self.failure_counter = collections.defaultdict(int)
+        self.logger = None
+
+    def run(self):
+        write_pool = ThreadPoolExecutor(max_workers=1, thread_name_prefix="TrackerWriteThread")
+        self.logger = logging.getLogger(__file__)
+        self.logger.setLevel(logging.DEBUG)
+        _console_handler = logging.StreamHandler()
+        _console_handler.setLevel(CONSOLE_LOG_LEVEL)
+        _console_handler.setFormatter(logging.Formatter("%(asctime)s - %(threadName)s : %(levelname)s : %(message)s"))
+        self.logger.addHandler(_console_handler)
+        track_folder = os.path.join(self.target_folder, "tracking")
+        os.makedirs(track_folder, exist_ok=True)
+        log_fname = os.path.join(track_folder, f"log_{datetime.now().strftime('%Y%m%d_%H-%M-%S')}.log")
+        log_file = RotatingFileHandler(log_fname, maxBytes=50_000_000, backupCount=10_000)
+        log_file.rotator = log_rotator
+        log_file.namer = log_namer
+        log_file.setLevel(FILE_LOG_LEVEL)
+        log_file.setFormatter(logging.Formatter("%(created)fs\t%(threadName)s\t%(levelname)s\t%(funcName)s:%(lineno)s\t%(index)d\t\"%(message)s\""))
+        self.logger.addHandler(log_file)
+        self.logger.addFilter(default_index_filter)
+        self.logger.info("Tracker initialized")
+        self.logger.info("Running with the following settings:")
+        self.logger.info(f"Pixel size: {self.pixel_size:.2f} µm")
+        self.logger.info(f"Frame rate: {self.frame_rate:.1f} fps")
+        self.logger.info(f"Minimum area: {self.min_area_pixels:.0f} pixels")
+        self.logger.info(f"Maximum area: {self.max_area_pixels:.0f} pixels")
+        self.logger.info(f"Regionprops: {self.regionprops}")
+        self.logger.info(f"Linking tracks: {self.link}")        
+
+        stopped = False
+        stop_after = None
+        max_idx = -1
+        self.start_time = time.time()
+        processed = 0
+        if cp:
+            self.cuda_stream = cp.cuda.Stream(non_blocking=True)
+        else:
+            self.cuda_stream = None
+        while not stopped or max_idx < stop_after:
+            task = self.track_tasks.get()  # blocks until task is available
+
+            if task['type'] == 'stop':
+                epoch = task.get('epoch', -1)
+                self.logger.info(f"Finishing up tracking tasks for epoch {epoch}")
+                if not task.get('discard', False):
+                    # make sure that all frames are processed
+                    write_pool.shutdown(wait=True)
+                    self.link_tracks(epoch, track_folder)
+                self.track_tasks.task_done()
+                if task['final']:
+                    stopped = True
+                    stop_after = task['last_idx']
+                else:
+                    # Create a new write pool
+                    write_pool = ThreadPoolExecutor(max_workers=1, thread_name_prefix="TrackerWriteThread")
+                continue
+
+            assert task['type'] == 'track', task
+            if task.get('discard', False):
+                self.track_tasks.task_done()
+                continue
+
+            processed += 1
+
+            try:
+                self.track_frame_objects(task, track_folder, write_pool)
+                self.track_tasks.task_done()
+                max_idx = max(task['idx'], max_idx)
+            except Exception:
+                self.logger.exception("Error while tracking objects, will try again", extra={'index': task['idx']})
+                self.failure_counter[task['idx']] += 1
+                self.track_tasks.task_done()
+                if self.failure_counter[task['idx']] < TRIES:
+                    self.track_tasks.put(task)  # re-queue the task
+                else:
+                    self.logger.error(f"Failed to track frame {task['idx']} after {TRIES} tries, giving up")
+                    max_idx = max(task['idx'], max_idx)
+
+        if not self.track_tasks.empty():
+            self.logger.error(f"There are still {self.track_tasks.qsize()} tasks in the queue")
+
+        self.logger.debug(
+            f"Processed {processed} frames in {time.time() - self.start_time:.2f} seconds, i.e. {processed / (time.time() - self.start_time):.1f} frames per second"
+        )
+
+    def track_frame_objects(self, task, track_folder, write_pool):
+        if self.cuda_stream:        
+            with self.cuda_stream:  # do not block other CUDA operations
+                idx, fname = task['idx'], task['fname']
+                self.logger.debug(f"Reading frame from '{fname}'", extra={'index': idx})
+                without_bg = cp.array(imageio.imread(fname))
+                properties = self.track_objects(idx, without_bg)
+        else:
+            idx, fname = task['idx'], task['fname']
+            self.logger.debug(f"Reading frame from '{fname}'", extra={'index': idx})
+            without_bg = np.array(imageio.imread(fname))
+            properties = self.track_objects(idx, without_bg)
+        self.logger.debug("Writing properties to file", extra={'index': idx})
+        df = pd.DataFrame(properties).rename(
+            columns={
+                "centroid-0": "y",
+                "centroid-1": "x",
+                "major_axis_length": "length",
+                "minor_axis_length": "width",
+                "orientation": "angle",
+            }
+        )
+        if self.columns is None:  # Note down columns once
+            self.columns = list(df.columns)
+        write_pool.submit(self.write_properties, track_folder, idx, df)
+        self.logger.debug("Wrote tracking data to 'tracking.tsv'", extra={'index': idx})
+        return idx
+
+    def write_properties(self, track_folder, idx, df):
+        tracking_fname = f"frame_{idx:07d}.tsv"
+        fname = os.path.join(track_folder, tracking_fname)
+        df.to_csv(
+            fname,
+            mode="w",
+            sep="\t",
+            index=False,
+            header=False,  # we don't write a header for easier merging later
+            float_format="%.2f",
+        )
+
+    def track_objects(self, idx, without_bg):
+        # 'labels' is just passed through to avoid re-allocating memory
+        self.logger.debug("Tracking objects", extra={'index': idx})
+        labels = determine_labels(without_bg, self.min_area_pixels, self.max_area_pixels)
+        images, objects, indices_all = determine_images(labels)
+        self.logger.debug("Extracting properties", extra={'index': idx})
+        properties = extract_properties(idx, images, objects, indices_all, self.regionprops)
+        self.logger.debug("Finished tracking/determining properties", extra={'index': idx})
+        return properties
+
+    def link_tracks(self, epoch, track_folder):
+        # Merge all files into one
+        if epoch != -1:
+            tracking_fname = os.path.join(track_folder, f"tracking_{epoch:07d}_unlinked_{self.frame_rate:.1f}_fps_{self.pixel_size:.2f}_um.tsv")
+        else:
+            tracking_fname = os.path.join(track_folder, f"tracking_unlinked_{self.frame_rate:.1f}_fps_{self.pixel_size:.2f}_um.tsv")
+        if not os.path.exists(tracking_fname):
+            self.logger.info(f"Linking tracks for epoch {epoch}, first concatenating all files")
+            track_files = sorted(glob.glob(os.path.join(track_folder, "frame_*.tsv")))
+            try:
+                with open(tracking_fname, "w") as merged_file:
+                    # Write header
+                    merged_file.write("\t".join(self.columns) + "\n")
+                    # Concatenate all files
+                    for track_file in track_files:
+                        with open(track_file, "r") as f:
+                            shutil.copyfileobj(f, merged_file)
+            except Exception:
+                self.logger.exception(f"Error while merging tracking files for epoch {epoch}, keeping individual files")
+                return
+        
+            self.logger.debug("Deleting individual files")
+            for track_file in track_files:
+                os.remove(track_file)
+        else:
+            self.logger.info(f"Linking tracks for epoch {epoch}, using existing file {tracking_fname}")
+
+        if self.link:
+            cells_df = pd.read_csv(tracking_fname, sep="\t")
+            try:
+                self.logger.info(f"Linking tracks for epoch {epoch}")
+                linked = self.link_wrapper(cells_df)
+                # Note that the filename states 1_um, since linked tracks are already scaled to µm                
+                if epoch != -1:
+                    linked_fname = os.path.join(track_folder, f"tracking_{epoch:07d}_linked_{self.frame_rate:.1f}_fps_1_um.tsv")
+                else:
+                    linked_fname = os.path.join(track_folder, f"tracking_linked_{self.frame_rate:.1f}_fps_1_um.tsv")
+                if self.zip_tracking_file:
+                    linked_fname += ".gz"
+                linked.to_csv(linked_fname, sep="\t", index=False, float_format="%.2f")
+                self.logger.info(f"linking tracks for epoch {epoch} done, linked tracks saved to {linked_fname}")
+            except Exception:
+                self.logger.exception(f"Linking tracks for epoch {epoch} failed")
+
+    def link_wrapper(self, df):
+        package = self.track_settings["package"]
+        settings = dict(self.track_settings[package])
+        # Preparations common to all packages
+        # 1. Scale the coordinates and lengths to µm
+        df[["x", "y", "bbox-0", "bbox-1", "bbox-2", "bbox-3"]] *= self.pixel_size
+        if "length" in df.columns:
+            df[["length", "width"]] *= self.pixel_size
+
+        # 2. Convert search range and memory to µm and frames
+        search_range = settings["maximum_speed"] / self.frame_rate
+        memory = int(round(settings["memory"] * self.frame_rate))
+        del settings["maximum_speed"]
+        del settings["memory"]
+        if package == "trackpy":
+            import trackpy as tp
+            if settings["adaptive_stop"] == 0:
+                adaptive_stop = None
+            else:
+                adaptive_stop = settings["adaptive_stop"] / self.frame_rate
+            del settings["adaptive_stop"]
+            self.logger.info(f"Linking tracks with Trackpy, search_range={search_range}, memory={memory}, adaptive_stop={adaptive_stop}, {' '.join(f'{k}={v}' for k, v in settings.items())}")
+            result = tp.link(df, search_range=search_range, memory=memory, adaptive_stop=adaptive_stop, **settings)
+            result.rename(columns={"particle": "id"}, inplace=True)
+
+        elif package == "norfair":
+            import norfair
+            initialization_delay = int(round(settings["initialization_delay"] * self.frame_rate))
+            del settings["initialization_delay"]
+            self.logger.info(f"Linking tracks with Norfair, distance_threshold={search_range}, hit_counter_max={memory}, initialization_delay={initialization_delay}, {' '.join(f'{k}={v}' for k, v in settings.items())}")
+            tracker = norfair.Tracker(
+                distance_function="mean_euclidean",
+                distance_threshold=search_range,
+                initialization_delay=initialization_delay,
+                hit_counter_max=memory,
+            )
+            output = []
+            for frame, rows in df.groupby('frame'):
+                norfair_detections = [norfair.Detection(points=np.array([row['x'], row['y']]), data=row) for _, row in rows.iterrows()]
+                tracked_objects = tracker.update(detections=norfair_detections)
+                for object in tracked_objects:
+                    last_detection = object.last_detection.data
+                    if last_detection['frame'] == frame: ## the last detection could be far in the past
+                        row = last_detection.to_dict()
+                        row.update({'id' : object.id})
+                        output.append(row)
+            result = pd.DataFrame(output)
+        else:
+            raise ValueError(f"Unknown tracking package '{package}'")
+        return result
